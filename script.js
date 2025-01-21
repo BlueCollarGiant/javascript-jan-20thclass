b73927a74b2eec952d2cb540e8105ba06e5d7571
BlueCollarGiant <167778887+BlueCollarGiant@users.noreply.github.com>
2025-01-20 18:30:38 -0600

    finishing classwork

---
+++ script.js
@@ -0,0 +1,24 @@
+/*console.log(true);
+console.log(false);
+
+console.log(Boolean(null));
+console.log(Boolean(undefined));
+console.log(Boolean(NaN));*/
+
+/*let age;
+
+if (age >= 18) {
+    console.log (" you are eligible to vote")
+} else {
+    console.log("your to young")
+}*/
+
+let isThursday= false;
+
+let isPayday = false;
+
+if (isThursday|| isPayday){
+    console.log("Time for class");
+} else {
+    console.log("woot payday");
+}
